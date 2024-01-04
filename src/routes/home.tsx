b00b3f744ae770b83a83f4a321dc112beb986246
dltmdska <dltmdska@naver.com>
2024-01-04 16:36:50 +0900

    Realtime Tweets

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,8 +1,12 @@
 import styled from "styled-components";
 import PostTweetForm from "../components/post-tweet-form";
+import Timeline from "../components/timeline";
 
 const Wrapper = styled.div`
-    
+    display: grid;
+    gap:50px;
+    overflow-y: scroll;
+    grid-template-rows: 1tr 5tr;
 `
 
 export default function Home(){
@@ -10,6 +14,7 @@ export default function Home(){
         <>
             <Wrapper>
                 <PostTweetForm/>
+                <Timeline/>
             </Wrapper>
         </>
     )
