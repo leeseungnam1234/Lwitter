70f519d9a82ad23f1a8eb42346a3077427f69d81
dltmdska <dltmdska@naver.com>
2024-01-05 16:49:10 +0900

    6:49

--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -1,8 +1,11 @@
 import styled from "styled-components"
-import { auth, storage } from "../firebase"
-import { useState } from "react"
+import { auth, db, storage } from "../firebase"
+import { useEffect, useState } from "react"
 import { getDownloadURL, ref, uploadBytes } from "firebase/storage"
 import { updateProfile } from "firebase/auth"
+import { collection, getDocs, limit, orderBy, query, where } from "firebase/firestore"
+import { ITweet } from "../components/timeline"
+import Tweet from "../components/tweet"
 
 const Wrapper = styled.div`
     display:flex;
@@ -32,10 +35,16 @@ const AvatarInput = styled.input`
 const Name = styled.span`
     font-size:22px;
 `
+const Tweets = styled.div`
+    display:flex;
+    flex-direction:column;
+    gap:10px;
+`
 
 export default function Profile(){
     const user = auth.currentUser
     const [avatar, setAvatar] = useState(user?.photoURL)
+    const [tweets, setTweets] = useState<ITweet[]>([])
     const onAvatarChange = async (e:React.ChangeEvent<HTMLInputElement>) =>{
         const {files} = e.target
         if (!user) return;
@@ -50,6 +59,30 @@ export default function Profile(){
             })
         }
     }
+    const fetchTweets = async() => {
+        const tweetQuery = query(
+            collection(db,'tweets'),
+            where('userId','==',user?.uid),
+            orderBy('createdAt','desc'),
+            limit(25)
+        )
+        const snapshot = await getDocs(tweetQuery)
+        const tweets = snapshot.docs.map(doc=>{
+            const {tweet, createdAt, userId, username, photo} = doc.data()
+                return{
+                    tweet,
+                    createdAt,
+                    userId,
+                    username, 
+                    photo,
+                    id: doc.id,
+                }
+        })
+        setTweets(tweets)
+    }
+    useEffect(()=>{
+        fetchTweets()
+    },[])
     return(
         <Wrapper>
             <AvatarUpload htmlFor='avatar'>
@@ -61,13 +94,16 @@ export default function Profile(){
                 </svg>)}
             </AvatarUpload>
             <AvatarInput 
-            onClick={onAvatarChange}
+            onChange={onAvatarChange}
             id="avatar" 
             type='file' 
             accept='image/*' />
             <Name>
                 {user?.displayName ?? '익명'}
             </Name>
+            <Tweets>
+                {tweets.map(tweet => <Tweet key={tweet.id} {...tweet}/>)}
+            </Tweets>
         </Wrapper>
     )
 }
